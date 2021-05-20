08284d37b25d6ce95c31dc944ef404f9e78eab31
NguyenDangLamPhuong <s3754105@rmit.edu.vn>
2021-05-20 18:06:46 +0700

    try git

--- sadi/src/main/java/rmit/university/sadi/SadiApplication.java
+++ sadi/src/main/java/rmit/university/sadi/SadiApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SadiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SadiApplication.class, args);
+		System.out.println("hello");
 	}
 
 }
